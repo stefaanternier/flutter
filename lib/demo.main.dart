c90cfa369ae41ad2b2164edfb013f8dbc8869bc7
Stefaan Ternier <stefaan@io-things.eu>
2021-12-02 08:56:27 +0100

    add documentation

--- lib/demo.main.dart
+++ lib/demo.main.dart
@@ -1,39 +1,36 @@
 import 'dart:async';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
 import 'package:uni_links/uni_links.dart';
+import 'package:universal_platform/universal_platform.dart';
 import 'package:youplay/config/app_config.dart';
 import 'package:youplay/models/models.dart';
-import 'package:youplay/screens/pages/home_page.dart';
+import 'package:youplay/router/route-widget.container.dart';
 import 'package:youplay/store/actions/auth.actions.dart';
 import 'package:youplay/store/actions/game_library.actions.dart';
+import 'package:youplay/store/actions/ui_actions.dart';
+import 'package:youplay/store/state/ui_state.dart';
 import 'package:youplay/store/store.dart';
 import 'package:youplay/themes/kien.dart';
 
 import 'config/login_config.dart';
-import 'localizations.dart';
-import 'themes/dido.dart';
+
 
 StreamSubscription? _sub;
 FirebaseAnalytics analytics = FirebaseAnalytics();
 
 
 Future<Null> initUniLinks(Store<AppState> store) async {
-  // Attach a listener to the stream
   _sub = getLinksStream().listen((String? link) {
     if (link != null) {
       store.dispatch(new ParseLinkAction(link: link));
     }
-  }, onError: (err) {
-  });
-
+  }, onError: (err) {});
 }
 
 
@@ -62,55 +59,41 @@ void main() async {
       });
 
   final store = await createStore();
-
   runApp(MyApp(store: store));
   FirebaseApp app = await Firebase.initializeApp();
   User? user = await FirebaseAuth.instance.currentUser;
-  print("user is ${user}");
+
+
   if (user != null) {
     String token = await user.getIdToken(true);
     store.dispatch(new CustomLoginSucceededAction(
         user.displayName, user.email, user.uid, user.isAnonymous));
+  } else {
+    store.dispatch(SetPage(page: PageType.intro));
   }
 
   store.dispatch(new LoadFeaturedGameAction());
   store.dispatch(new LoadRecentGamesAction());
   await initUniLinks(store);
   String? link = await getInitialLink();
-
   if (link != null) {
     store.dispatch(new ParseLinkAction(link: link));
-  } else {
-
-
   }
 }
 
 class MyApp extends StatelessWidget {
   final Store<AppState> store;
 
-  MyApp({Key? key,required this.store}) : super(key: key);
+  MyApp({Key? key, required this.store}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return new StoreProvider(
         store: store,
         child: Center(
-          child: new MaterialApp(
-            localizationsDelegates: [
-              AppLocalizations.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate
-            ],
-            supportedLocales: [const Locale('nl'), const Locale('en')],
-            debugShowCheckedModeBanner: false,
-            title: AppConfig().appName ?? 'todo title',
-            theme: AppConfig().themeData,
-            home: new SplashScreen(),
-            navigatorObservers: [
-              FirebaseAnalyticsObserver(analytics: analytics),
-            ],
-          ),
+            child: RouteWidgetContainer(
+              key: ValueKey('RouterWidgetContainer'),
+            )
         ));
   }
 }
